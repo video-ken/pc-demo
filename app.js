d72263cacb4f1b00cb179c48af1f3cfc1de105d6
Jeril Sebastian <seb988@gmail.com>
2020-09-24 08:01:50 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,57 @@
+import phrases from "./data";
+
+Object.entries(phrases).forEach(([time, text], index) => {
+    let word = document.createElement('span');
+    word.classList.add("word");
+    word.textContent = text;
+    word.setAttribute("data-time", time);
+    let dir = Math.max(0, 26 - text.length);
+    word.setAttribute("data-dir", index % 2 === 0 ? dir : -1 * dir);
+    word.style.left = Math.max((40 - text.length), 10) + '%';
+    preview.append(word);
+});
+
+let listener;
+
+function movePreview(x) {
+    let percentage = x/8;
+    let left = x - 80;
+    preview.style.transform = `translateX(${left}px)`;
+
+    let words = preview.querySelectorAll(".word");
+
+    for(let i=0; i<words.length; i++) {
+        let word = words[i];
+        let time = parseInt(word.getAttribute('data-time'));
+        let dir = parseInt(word.getAttribute('data-dir'));
+        console.log(dir);
+        
+        if(time > percentage - 10 && time < percentage + 10) {
+            let difference = Math.abs(percentage - time);
+            word.style.opacity = 1;
+            word.style.transform = `translate(${dir}px, ${(percentage - time)*5}px) scale(${2 - difference/5})`;
+        }
+        else {
+            word.style.opacity = 0;
+        }
+    }
+}
+
+timeline.addEventListener("pointerover", _ => {
+    preview.style.opacity = 1;
+
+    listener = timeline.addEventListener("pointermove", event => {
+        let offset = parseInt(event.offsetX);
+
+        requestAnimationFrame(() => {
+            movePreview(offset);
+        });
+
+    });
+});
+
+
+timeline.addEventListener("pointerout", _ => {
+    timeline.removeEventListener("pointermove", listener);
+    preview.style.opacity = 0;
+});
---
+++ data.js
@@ -0,0 +1,18 @@
+export  default {
+    "6" : "datasets",
+    "10" : "machine learning",
+    "29" : "predictive models",
+    "24" : "auto encoder",
+    "39" : "backpropagation",
+    "44" : "supervised learning",
+    "50" : "python",
+    "53" : "dataset",
+    "58" : "deep learning",
+    "64" : "bell labs",
+    "68" : "convolutional neural network",
+    "74" : "image recognition",
+    "78" : "auto encoder",
+    "88" : "deep neural networks",
+    "95" : "stocastic gradient descent",
+    "100" : "self driving cars"
+}
